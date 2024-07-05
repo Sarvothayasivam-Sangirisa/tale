9761898a9a885323952f5501f118945bf8f6d493
Sarvothayasivam-Sangirisa <sangiri15sivam@gmail.com>
2024-07-05 12:47:28 +0530

    fistadd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from '../src/compontens/Header.js';
+import Section from '../src/compontens/Section.js';
+import Footer from '../src/compontens/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <Header/>
+
+    <Section/>
+    
+    <Footer/>
+    </>
   );
 }
 
+
+
 export default App;
+
